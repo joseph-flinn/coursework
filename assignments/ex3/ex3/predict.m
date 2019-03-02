2646413a4b0909ab1d74d01822e5f40fc1ad24a1
Joseph <joeflinn412@gmail.com>
2019-03-02 10:25:00 -0800

    finished ex3

--- assignments/ex3/ex3/predict.m
+++ assignments/ex3/ex3/predict.m
@@ -21,13 +21,9 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m, 1) X];
 
-
-
-
-
-
-
+[prob, p] = max(sigmoid(Theta2 * [ones(1, m); (sigmoid(Theta1 * X'))])', [], 2);
 
 % =========================================================================
 
