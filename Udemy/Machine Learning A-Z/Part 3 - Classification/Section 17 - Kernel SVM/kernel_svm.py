3ebce74263827fff94b6672a9de935ad3cb7d47f
Joseph Flinn <joeflinn412@gmail.com>
2019-11-15 10:18:11 -0800

    added more ML course notes

--- Udemy/Machine Learning A-Z/Part 3 - Classification/Section 17 - Kernel SVM/kernel_svm.py
+++ Udemy/Machine Learning A-Z/Part 3 - Classification/Section 17 - Kernel SVM/kernel_svm.py
@@ -23,36 +23,31 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 
-# Fitting Logisitc Regression to the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state=0)
+# Fitting the classifier to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel='rbf', random_state=0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-# Metrics
-from sklearn.metrics import confusion_matrix, roc_auc_score, classification_report
-print("\nClassification report for classifier %s:\n\n%s\n" % (
-        classifier, classification_report(y_test, y_pred)))
-print("Area under curve (ROC): %s\n" % roc_auc_score(y_test, y_pred))
-print("Confusion Matrix:\n%s)" % confusion_matrix(y_test, y_pred))
-
-
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 
 # Visualizing the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contour(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X1.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], 
                 c = ListedColormap(('red', 'green'))(i), label=j)
-plt.title('Logistic Regression (Training set)')
+plt.title('Classifier (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -69,9 +64,8 @@ plt.ylim(X2.min(), X1.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], 
                 c = ListedColormap(('red', 'green'))(i), label=j)
-plt.title('Logistic Regression (Test set)')
+plt.title('Classifier (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-                     )
--- Udemy/Machine Learning A-Z/Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classifier.py
+++ Udemy/Machine Learning A-Z/Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classifier.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 17 13:00:28 2019
+Created on Thu Sep 19 11:07:47 2019
 
 @author: Joseph
 """
@@ -23,36 +23,31 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 
-# Fitting Logisitc Regression to the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state=0)
+# Fitting the classifier to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=10, criterion="entropy")
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-# Metrics
-from sklearn.metrics import confusion_matrix, roc_auc_score, classification_report
-print("\nClassification report for classifier %s:\n\n%s\n" % (
-        classifier, classification_report(y_test, y_pred)))
-print("Area under curve (ROC): %s\n" % roc_auc_score(y_test, y_pred))
-print("Confusion Matrix:\n%s)" % confusion_matrix(y_test, y_pred))
-
-
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 
 # Visualizing the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contour(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X1.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], 
                 c = ListedColormap(('red', 'green'))(i), label=j)
-plt.title('Logistic Regression (Training set)')
+plt.title('Random Forest Classifier (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -69,9 +64,8 @@ plt.ylim(X2.min(), X1.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], 
                 c = ListedColormap(('red', 'green'))(i), label=j)
-plt.title('Logistic Regression (Test set)')
+plt.title('Random Forest Classifier (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-                     )
